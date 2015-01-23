afacd301fd576b4acee7dea7e707dafff6f173ca
rajesh <e21rajesh@gmail.com>
2015-01-23 01:06:41 -0500

    working script

--- build_k.sh
+++ build_k.sh
@@ -1,43 +1,47 @@
 #!/bin/bash -f
 
 if [ "x$1" = "x" -o "x$2" = "x" -o "x$3" = "x" ];then
-        echo "use: $0 <git_dev_branch> <git_rel_branch> <tag_name>"
+        echo "Usage: $0 <git_dev_branch> <git_rel_branch> <tag_name>"
         exit 1
 fi
 
 today=`date +%Y-%m-%d_%H%M%S`
-LOG_FILE=c:/swethaw/merge_$today.log
+LOG_FILE=c:/swethaw/merge_$1_$2_$today.log
 echo "Start of Build...." >> $LOG_FILE
 
-git branch -a | grep -E '^\*' | awk '{print $2}'  > c:/swethaw/xabc
+DEV_FOUND="NO"
+#git branch -a | grep -E '^\*' | awk '{print $2}'  > c:/swethaw/xabc
 #sed -i 's/*//g' c:/swethaw/xabc
-while read branch
+#while read branch
+for branch in $(git branch -a | sed 's/* //g' | sed 's/  //g')
 do
-	echo "Checking branch $branch" >>$LOG_FILE
+	echo "Checking $branch for matching dev branch $1" >>$LOG_FILE   # THIS print can be deleted later
 	if [ "$1" = "$branch" ]; then
+		DEV_FOUND="YES"
+		echo "Dev branch $1 found in the branches" >> $LOG_FILE
 		git checkout $1
 		if [ "$?" = 0 ]; then
-			echo "Dev branch checkout success" >>$LOG_FILE
+			echo "Dev branch $1 checkedout successfully" >>$LOG_FILE
 			git pull				 
 			if [ "$?" = 0 ]; then
-				echo "git pull success" >>$LOG_FILE
+				echo "git pull is done successfully" >>$LOG_FILE
 				RELEASE_FOUND="NO"
-				git branch -a | grep -E '^\*' | awk '{print $2}' > c:/swethaw/rabc
-				sed -i 's/*//g' c:/swethaw/rabc
+				#git branch -a | grep -E '^\*' | awk '{print $2}' > c:/swethaw/rabc				
+				#sed -i 's/*//g' c:/swethaw/rabc
 				
 				#this loop is to check whether the release branch exist or not
-				while read relbranch
+				#while read relbranch
+				for relbranch in $(git branch -a | sed 's/* //g' | sed 's/  //g')
 				do
-					echo "Checking Release branch $relbranch" >>$LOG_FILE
+					echo "Checking $relbranch for matching release branch $2" >>$LOG_FILE # THIS print can be deleted later
 					if [ "$2" = "$relbranch" ] ; then
 						RELEASE_FOUND="YES"
 						echo "Release branch $2 found" >>$LOG_FILE
 						git checkout $2
 						if [ "$?" = 0 ]; then
-							echo "Checkout to release branch success">>$LOG_FILE
+							echo "Checkout to release branch is successful">>$LOG_FILE
 							RESULT=`git merge $1`
-							echo "$RESULT"  >> $LOG_FILE
-							if [ "$RESULT" = "Merged to release branch $2" ]; then
+							if [ "$RESULT" == "Merged to release branch $2" ]; then
 								git push                                                                
 								TAG_FOUND="NO"
 								#Check if the tag exists in git
@@ -52,7 +56,7 @@ do
 								done
 								
 								#Delete the tag if it is already present
-								if [$found="YES"]; then
+								if [ $TAG_FOUND == "YES" ]; then
 									git tag -d $3
 									git push -u origin tag :$3
 									echo "Successfully deleted the tag $3" >>$LOG_FILE
@@ -61,8 +65,11 @@ do
 								git tag $3
 								git push origin tag $3
 								echo "Successfully created the tag and merged the code to release branch" >>$LOG_FILE
-							else
-								echo "conflicts found while merging" >> $LOG_FILE
+							elif [ "$RESULT" == "Already up-to-date." ]; then
+								echo "Result : $RESULT" >> $LOG_FILE
+							else								
+								echo "Result : $RESULT" >> $LOG_FILE
+								echo "Conflicts found while merging to $2" >> $LOG_FILE
 								exit 1
 							fi
 						else
@@ -71,9 +78,11 @@ do
 						fi
 						break
 					fi
-				done < c:/swethaw/rabc					
+				#done < c:/swethaw/xyz					
+				done
 				
-				if [ $RELEASE_FOUND="YES" ]; then
+				if [ $RELEASE_FOUND == "YES" ]; then
+					echo "Release branch $2 found and done with merge task" >> $LOG_FILE
 					break
 				else
 					echo "Release branch $2 not found. Creating the release branch" >> $LOG_FILE
@@ -92,6 +101,11 @@ do
 		echo "Done with the script" >> $LOG_FILE
 		exit 1
 	fi
-done < c:/swethaw/xabc
+#done < c:/swethaw/abc
+done
+
+if [ $DEV_FOUND == "NO" ]; then
+	echo "Dev branch $1 is not found" >>$LOG_FILE
+fi
 echo "Bye" >> $LOG_FILE
 
